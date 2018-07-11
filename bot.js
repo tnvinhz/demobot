8042db672c19dc5a20ad90f8b921dcd544a78245
vinhtn <vinhtn@mobgame.vn>
2018-07-11 14:45:38 +0700

    cm
    cm

--- bot.js
+++ bot.js
@@ -15,5 +15,5 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());  
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:') 
 var bot = new builder.UniversalBot(connector, function (session) {     
-session.send("You said: %s", session.message.text); 
+session.send("Xúy xúy xúy", session.message.text); 
 });
