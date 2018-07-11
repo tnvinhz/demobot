e757d8526c6ed63b60d66de88a100d904912873f
vinhtn <vinhtn@mobgame.vn>
2018-07-11 17:16:05 +0700

    cm
    cm

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var builder = require('botbuilder');
 
 var MICROSOFT_APP_ID = '85827e55-6e1b-41e8-aadf-1048971067be';
 var MICROSOFT_APP_PASSWORD = 'lE(Rr?aEa&v1&B=w';
-var port = process.env.PORT || 1337
 // Setup Restify Server 
 var server = restify.createServer(); 
-server.listen(port, 
+server.listen(process.env.port || process.env.PORT || 1337, 
 function () {    
     console.log('%s listening to %s', server.name, server.url);  
 });  
