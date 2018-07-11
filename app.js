9c4b1ee2b0fa2bc6de60ae9dfcb8d5d71a617ab3
vinhtn <vinhtn@mobgame.vn>
2018-07-11 17:18:33 +0700

    cm
    cm

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-﻿"use strict";
-var restify = require('restify');
-var builder = require('botbuilder');
-
-var MICROSOFT_APP_ID = '85827e55-6e1b-41e8-aadf-1048971067be';
-var MICROSOFT_APP_PASSWORD = 'lE(Rr?aEa&v1&B=w';
+﻿var restify = require('restify'); 
+var builder = require('botbuilder');  
 // Setup Restify Server 
 var server = restify.createServer(); 
-server.listen(process.env.port || process.env.PORT || 1337, 
+server.listen(process.env.port || process.env.PORT || 3978, 
 function () {    
     console.log('%s listening to %s', server.name, server.url);  
 });  
 // chat connector for communicating with the Bot Framework Service 
 var connector = new builder.ChatConnector({     
-    appId: MICROSOFT_APP_ID,     
-    appPassword: MICROSOFT_APP_PASSWORD
+    appId: '85827e55-6e1b-41e8-aadf-1048971067be',     
+    appPassword: 'lE(Rr?aEa&v1&B=w'
 });
 // Listen for messages from users  
 server.post('/api/messages', connector.listen());  
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:') 
 var bot = new builder.UniversalBot(connector, function (session) {     
-session.send("hiiiiiiiiiii", session.message.text); 
+session.send("You said: %s", session.message.text); 
 });
